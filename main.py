8fe428ce55f174ae55f725e60f05ba7f163517f0
Adrian Morgenthal <Adrian.Morgenthal@haw-hamburg.de>
2024-06-25 00:20:13 +0200

    Implement toggle chatbox

--- main.py
+++ main.py
@@ -174,11 +174,16 @@ if st.button('LaTeX'):
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# Toggle Chatbox
+show_chat = st.checkbox("Show Chatbox")
 
-prompt = st.chat_input("Write your message")
-if prompt:
-    st.write(f"User: {prompt}")
+if show_chat:
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    prompt = st.chat_input("Write your message")
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.write(f"User: {prompt}")
