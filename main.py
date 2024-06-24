ce764bbea6a34eed44402a7706f42b239ad5fa92
Adrian Morgenthal <Adrian.Morgenthal@haw-hamburg.de>
2024-06-25 00:32:21 +0200

    Refactor chatbox to page1

--- main.py
+++ main.py
@@ -173,20 +173,3 @@ if st.button('LaTeX'):
         a \left(\frac{1-x^{n}}{1-x}\right)
         ''')
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Toggle Chatbox
-show_chat = st.checkbox("Show Chatbox")
-
-if show_chat:
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    prompt = st.chat_input("Write your message")
-    if prompt:
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        st.write(f"User: {prompt}")
--- pages/page1.py
+++ pages/page1.py
@@ -1,6 +1,27 @@
 import streamlit as st
 
 
+st.set_page_config(page_title="DataVizApp", layout="wide")
+
+
 def page1():
-    st.title("Page 1")
-    st.write("Content for page 1")
+    st.title("Chat")
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Toggle Chatbox
+show_chat = st.checkbox("Show Chatbox")
+
+if show_chat:
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    prompt = st.chat_input("Write your message")
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.write(f"User: {prompt}")
